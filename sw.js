db17a66eedbb86e297762dcae5bad281df52ce70
safaomrafr-svg <safaomra.fr@gmail.com>
2026-07-14 21:54:52 +0100

    Bump cache v14

--- sw.js
+++ sw.js
@@ -1,27 +1,5 @@
-const CACHE = 'safa-omra-v13';
-
-// Chemins RELATIFS (restent dans /safa-omra-app/)
+const CACHE = 'safa-omra-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('script.google.com')) return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
-
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { if (e.request.url.includes('script.google.com')) return; e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
