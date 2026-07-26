97e6bd51c8132ba836ab3cb7a354daa4924d5d68
safaomrafr-svg <safaomra.fr@gmail.com>
2026-07-26 17:19:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'safa-omra-v25';
+const CACHE = 'safa-omra-v26';
 
 // Chemins RELATIFS (restent dans /safa-omra-app/)
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
