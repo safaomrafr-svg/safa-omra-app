1bc5986d0febd40ab96ccf56b8fdb1f44e567293
safaomrafr-svg <safaomra.fr@gmail.com>
2026-07-31 09:54:17 +0100

    Update service worker cache version to v59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'safa-omra-v58';
+const CACHE = 'safa-omra-v59';
 
 // Chemins RELATIFS (restent dans /safa-omra-app/)
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
