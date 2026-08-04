94626b70f92eb3c9cf223504358c0ebb00090189
safaomrafr-svg <safaomra.fr@gmail.com>
2026-08-04 19:23:03 +0100

    Update cache version from v88 to v89

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'safa-omra-v88';
+const CACHE = 'safa-omra-v89';
 
 // Chemins RELATIFS (restent dans /safa-omra-app/)
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
